a75683a056ec9975f615b3b0a8609aeb834a05c7
Neil Arnold <neilparnold@gmail.com>
2020-12-04 12:50:35 -0500

    minor edits

--- app/setup.php
+++ app/setup.php
@@ -11,8 +11,8 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+    wp_enqueue_style( 'sage/main.css', asset_path('styles/main.css'), false, null );
+    wp_enqueue_script( 'sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true );
     wp_enqueue_style( 'fontawesome', 'https://use.fontawesome.com/releases/v5.10.2/css/all.css', array(), '5.10.2' );
     wp_enqueue_style( 'typekit', 'https://use.typekit.net/djf7stt.cs', array(), '1.0.0' );
 
