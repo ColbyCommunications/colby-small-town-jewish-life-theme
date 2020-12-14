338c0d89f883575ad48052ee902ade8db50b52f2
Neil Arnold <neilparnold@gmail.com>
2020-12-14 17:12:29 -0500

    fixed typo in typekit call

--- app/setup.php
+++ app/setup.php
@@ -14,7 +14,7 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style( 'sage/main.css', asset_path('styles/main.css'), false, null );
     wp_enqueue_script( 'sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true );
     wp_enqueue_style( 'fontawesome', 'https://use.fontawesome.com/releases/v5.10.2/css/all.css', array(), '5.10.2' );
-    wp_enqueue_style( 'typekit', 'https://use.typekit.net/djf7stt.cs', array(), '1.0.0' );
+    wp_enqueue_style( 'typekit', 'https://use.typekit.net/djf7stt.css', array(), '1.0.0' );
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
